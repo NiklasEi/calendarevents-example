459c759d407e75b48941739031801df7b983ff13
Niklas Eicker <nikle1@gmx.de>
2018-03-15 21:44:26 +0100

    update to CalendarEvents 1.4.0

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# ExampleCalendarEventsAPI
+# CalendarEvents example usage
 
-This project is to demonstrate the usage of CalendarEventsAPI. It is a functioning Bukkit plugin that has [CalendarEvents] as a dependency and will register a Calendar event to get called 2 minutes after server start and on two other hardcoded timings.
+This project demonstrates the usage of the CalendarEvents API. It is a functioning Bukkit plugin that has [CalendarEvents] as a dependency and registers a calendar event to get called 2 minutes after server start and on two other hardcoded, repeating timings.
 
-As an example of how to listen for a CalendarEvent, a message will be printed to the console from a listener when the custom event is called.
+As an example of how to listen for a CalendarEvent, a message is printed to the console by a listener when the custom event is called.
 
 
 [CalendarEvents]: https://github.com/NiklasEi/CalendarEvents
--- src/main/java/me/nikl/CalendarEventsExample.java
+++ src/main/java/me/nikl/CalendarEventsExample.java
@@ -1,6 +1,7 @@
-package me.nikl.examplecalendareventsapi;
+package me.nikl;
 
-import me.nikl.calendarevents.APICalendarEvents;
+import me.nikl.calendarevents.CalendarEvents;
+import me.nikl.calendarevents.CalendarEventsApi;
 import me.nikl.calendarevents.CalendarEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -18,35 +19,36 @@ import java.util.logging.Level;
  *
  * See CalendarEvents: https://github.com/NiklasEi/CalendarEvents
  */
-public class Main extends JavaPlugin implements Listener{
-    private APICalendarEvents api;
+public class CalendarEventsExample extends JavaPlugin implements Listener{
+    private CalendarEventsApi api;
     private String uniqueEventLabel = UUID.randomUUID().toString();
 
     public void onEnable(){
-        me.nikl.calendarevents.Main calendarEvents = (me.nikl.calendarevents.Main) Bukkit.getPluginManager().getPlugin("CalendarEvents");
-
+        CalendarEvents calendarEvents = (CalendarEvents) Bukkit.getPluginManager().getPlugin("CalendarEvents");
         if(calendarEvents == null || !calendarEvents.isEnabled()){
             Bukkit.getLogger().log(Level.SEVERE, " CalendarEvents is not running!");
             return;
         }
+        Bukkit.getPluginManager().registerEvents(this, this);
+        registerCalendarEvent(calendarEvents);
+    }
 
+    private void registerCalendarEvent(CalendarEvents calendarEvents) {
         // get the time in two minutes in the format 'hh:mm'
         ZonedDateTime now = ZonedDateTime.now();
         ZonedDateTime timeInTwoMin = now.plusMinutes(2);
+        // convert time in two min to hh:mm
         String inTwoMin = (timeInTwoMin.getHour() < 10 ? "0" + timeInTwoMin.getHour() : String.valueOf(timeInTwoMin.getHour()))
                 + ":" + ((timeInTwoMin.getMinute())< 10 ? "0" + (timeInTwoMin.getMinute()) : String.valueOf(timeInTwoMin.getMinute()));
-
         // get the API
         api = calendarEvents.getApi();
-
-        // we will now add an event that will be fired 2 minutes after server start and at two different timings just to show you what is possible
+        // we will now add an event that will be fired 2 minutes after server start and at two different timings
+        // the last two parameters are the same, when you add this event in the configuration file in CalendarEvents (occasion and timing)
         if(api.addEvent(uniqueEventLabel, "every day", inTwoMin + ", 17:45, 21:12")) {
             getLogger().info(" An event with the occasions: every day       and the timings: " + inTwoMin + ", 17:45, 21:12     was added");
         } else {
             getLogger().info(" Failed adding the event. The label might already be in use...");
         }
-
-        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
@@ -59,10 +61,8 @@ public class Main extends JavaPlugin implements Listener{
         getLogger().info(" Example event was fired! Server time: " + event.getTime());
     }
 
-
     public void onDisable(){
         // You should remove your events here.
         api.removeEvent(uniqueEventLabel);
     }
-
 }
